eb92e7dba7ab61b839bf6cf098d3e93133fe71c7
Jake Wharton <jw@squareup.com>
2021-08-31 22:52:06 -0400

    Explicitly control time and frames for deterministic tests

--- molecule/molecule-runtime/src/androidTest/java/app/cash/molecule/MoleculeTest.kt
+++ molecule/molecule-runtime/src/androidTest/java/app/cash/molecule/MoleculeTest.kt
@@ -15,34 +15,37 @@
  */
 package app.cash.molecule
 
+import androidx.compose.runtime.BroadcastFrameClock
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import app.cash.molecule.AndroidUiDispatcher.Companion.Main
-import app.cash.turbine.test
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.junit.Assert.assertEquals
 import org.junit.Assert.fail
 import org.junit.Test
 import kotlin.coroutines.CoroutineContext
 import kotlin.time.ExperimentalTime
 
+@ExperimentalCoroutinesApi
 @ExperimentalTime
 class MoleculeTest {
-  @Test fun items() = runBlocking {
-    val scope = CoroutineScope(Main)
+  @Test fun items() {
+    val dispatcher = TestCoroutineDispatcher()
+    val clock = BroadcastFrameClock()
+    val scope = CoroutineScope(dispatcher + clock)
 
     val flow = scope.launchMolecule {
       var count by remember { mutableStateOf(0) }
       LaunchedEffect(Unit) {
         while (true) {
-          delay(100) // TODO control time and frames in the test!
+          delay(100)
           count++
         }
       }
@@ -52,18 +55,27 @@ class MoleculeTest {
 
     assertEquals(0, flow.value)
 
-    flow.test {
-      assertEquals(0, awaitItem())
-      assertEquals(1, awaitItem())
-      assertEquals(2, awaitItem())
-      cancel()
-    }
+    clock.sendFrame(0)
+    assertEquals(0, flow.value)
+
+    dispatcher.advanceTimeBy(99)
+    clock.sendFrame(0)
+    assertEquals(0, flow.value)
+
+    dispatcher.advanceTimeBy(1)
+    clock.sendFrame(0)
+    assertEquals(1, flow.value)
+
+    dispatcher.advanceTimeBy(100)
+    clock.sendFrame(0)
+    assertEquals(2, flow.value)
 
     scope.cancel()
   }
 
-  @Test fun errorImmediately() = runBlocking {
-    val scope = CoroutineScope(Main)
+  @Test fun errorImmediately() {
+    val clock = BroadcastFrameClock()
+    val scope = CoroutineScope(clock)
 
     val runtimeException = RuntimeException()
     try {
@@ -87,9 +99,11 @@ class MoleculeTest {
     override val key get() = CoroutineExceptionHandler
   }
 
-  @Test fun errorDelayed() = runBlocking {
+  @Test fun errorDelayed() {
+    val dispatcher = TestCoroutineDispatcher()
+    val clock = BroadcastFrameClock()
     val exceptionHandler = RecordingExceptionHandler()
-    val scope = CoroutineScope(Main + exceptionHandler)
+    val scope = CoroutineScope(dispatcher + clock + exceptionHandler)
 
     val runtimeException = RuntimeException()
     var count by mutableStateOf(0)
@@ -102,26 +116,20 @@ class MoleculeTest {
 
     assertEquals(0, flow.value)
 
-    flow.test {
-      assertEquals(0, awaitItem())
-
-      count++
-      delay(100) // TODO control frames!
-      assertEquals(listOf(runtimeException), exceptionHandler.exceptions)
-
-      expectNoEvents()
-      cancel()
-    }
+    count++
+    clock.sendFrame(0)
+    assertEquals(listOf(runtimeException), exceptionHandler.exceptions)
 
     scope.cancel()
   }
 
-  @Test fun errorInEffect() = runBlocking {
+  @Test fun errorInEffect() {
+    val dispatcher = TestCoroutineDispatcher()
+    val clock = BroadcastFrameClock()
     val exceptionHandler = RecordingExceptionHandler()
-    val scope = CoroutineScope(Main + exceptionHandler)
+    val scope = CoroutineScope(dispatcher + clock + exceptionHandler)
 
     val runtimeException = RuntimeException()
-    var count by mutableStateOf(0)
     val flow = scope.launchMolecule {
       LaunchedEffect(Unit) {
         delay(50)
@@ -132,15 +140,9 @@ class MoleculeTest {
 
     assertEquals(0, flow.value)
 
-    flow.test {
-      assertEquals(0, awaitItem())
-
-      delay(100) // TODO control frames!
-      assertEquals(listOf(runtimeException), exceptionHandler.exceptions)
-
-      expectNoEvents()
-      cancel()
-    }
+    dispatcher.advanceTimeBy(50)
+    clock.sendFrame(0)
+    assertEquals(listOf(runtimeException), exceptionHandler.exceptions)
 
     scope.cancel()
   }
