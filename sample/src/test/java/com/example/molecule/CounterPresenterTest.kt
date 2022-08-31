1f24c6535cc2d3a1af574595da512f58edd2b074
Igor Bozin <igorbzin@gmail.com>
2022-08-31 16:59:19 +0200

    Fix import order again

--- sample/src/test/java/com/example/molecule/CounterPresenterTest.kt
+++ sample/src/test/java/com/example/molecule/CounterPresenterTest.kt
@@ -18,9 +18,9 @@ package com.example.molecule
 import app.cash.molecule.RecompositionClock
 import app.cash.molecule.moleculeFlow
 import app.cash.turbine.test
-import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
