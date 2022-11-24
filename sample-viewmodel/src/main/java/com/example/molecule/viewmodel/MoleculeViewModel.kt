e5399420a98c2efc0428bcf1ca262b399678bd2e
Chris Horner <chorner@squareup.com>
2022-11-24 17:00:08 +1100

    Add foundation of sample that uses AAC ViewModel

---
+++ sample-viewmodel/src/main/java/com/example/molecule/viewmodel/MoleculeViewModel.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2022 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.molecule.viewmodel
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.AndroidUiDispatcher
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.cash.molecule.RecompositionClock.ContextClock
+import app.cash.molecule.launchMolecule
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
+abstract class MoleculeViewModel<Event, Model> : ViewModel() {
+  private val scope = CoroutineScope(viewModelScope.coroutineContext + AndroidUiDispatcher.Main)
+
+  // Events have a capacity large enough to handle simultaneous UI events, but
+  // small enough to surface issues if they get backed up for some reason.
+  private val events = MutableSharedFlow<Event>(extraBufferCapacity = 20)
+
+  val models: StateFlow<Model> by lazy(LazyThreadSafetyMode.NONE) {
+    scope.launchMolecule(clock = ContextClock) {
+      models(events)
+    }
+  }
+
+  fun take(event: Event) {
+    if (!events.tryEmit(event)) {
+      error("Event buffer overflow.")
+    }
+  }
+
+  @Composable
+  protected abstract fun models(events: Flow<Event>): Model
+}
---
+++ sample-viewmodel/src/main/java/com/example/molecule/viewmodel/ui.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2022 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.molecule.viewmodel
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RandomDogScreen(model: Model) {
+  Surface(modifier = Modifier.fillMaxSize()) {
+    Column {
+      Text("TODO: Build sample that hits https://dog.ceo/dog-api/")
+    }
+  }
+}
